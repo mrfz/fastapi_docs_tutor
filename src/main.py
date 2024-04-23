1f187d08a9d5cf63bcc78b5dd6693cced373207c
mrfz <zlobin.f@gmail.com>
2024-04-23 22:24:10 +0300

    feat: add model name request with enum response

--- src/main.py
+++ src/main.py
@@ -1,6 +1,26 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
+
+class ModelName(str, Enum):
+    """Class for enum test
+
+    Args:
+        str (str): string name
+        Enum (Enum): enumerated value
+
+    Returns:
+        class: ModelName class
+    """
+
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
 app = FastAPI()
+
 
 @app.get("/items/{item_id}")
 async def root(item_id: int) -> dict[str, int]:
@@ -14,11 +34,12 @@ async def root(item_id: int) -> dict[str, int]:
     """
     return {"item_id": item_id}
 
+
 # Order matters!
 @app.get("/users/me")
 async def read_user_me() -> dict[str, str]:
     """Response to a GET request
-    
+
     Returns:
         dict[str, str]: dict response
     """
@@ -28,7 +49,7 @@ async def read_user_me() -> dict[str, str]:
 @app.get("/users/{username}")
 async def read_user(username: str) -> dict[str, str]:
     """Response to a GET request
-    
+
     Args:
         username (str): username to return
 
@@ -36,3 +57,22 @@ async def read_user(username: str) -> dict[str, str]:
         dict[str, str]: dict response
     """
     return {"username": username}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName) -> dict[str, str]:
+    """Response to a GET model request
+
+    Args:
+        model_name (ModelName): model name to return
+
+    Returns:
+        dict[str, str]: dict response
+    """
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
