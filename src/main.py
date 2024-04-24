d2391d0b8de8bbd97e5e201f646ad63eef75414b
mrfz <zlobin.f@gmail.com>
2024-04-24 21:06:00 +0300

    feat: add query params
    https://fastapi.tiangolo.com/tutorial/query-params/

--- src/main.py
+++ src/main.py
@@ -2,6 +2,45 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
 @app.get("/")
 async def root() -> dict[str, str]:
     return {"message": "Hello World"}
+
+
+@app.get("/items/")
+async def read_items(skip: int = 0, limit: int = 10) -> list[dict[str, str]]:
+    """Get all items
+
+    Args:
+
+        skip (int, optional): skip first n items. Defaults to 0.
+
+        limit (int, optional): maximum number of items. Defaults to 10.
+    Returns:
+
+        list[dict[str, str]]: list of items
+    """
+
+    return fake_items_db[skip : skip + limit]
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None) -> dict[str, str]:
+    """Get an item
+
+    Args:
+
+        item_id (str): item id
+
+        q (str | None, optional): query. Defaults to None.
+
+    Returns:
+
+        dict[str, str]: item
+    """
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
