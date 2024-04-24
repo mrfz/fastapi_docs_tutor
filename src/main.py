fb198a6d4ebbcb58ef9dc318e35fe0f0f88426bc
mrfz <zlobin.f@gmail.com>
2024-04-24 21:34:33 +0300

    feat: add bool query param

--- src/main.py
+++ src/main.py
@@ -2,45 +2,52 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+fake_items_db = [{'item_name': 'Foo'}, {'item_name': 'Bar'}, {'item_name': 'Baz'}]
 
 
-@app.get("/")
+@app.get('/')
 async def root() -> dict[str, str]:
-    return {"message": "Hello World"}
+	return {'message': 'Hello World'}
 
 
-@app.get("/items/")
+@app.get('/items/')
 async def read_items(skip: int = 0, limit: int = 10) -> list[dict[str, str]]:
-    """Get all items
+	"""Get all items
 
-    Args:
+	Args:
 
-        skip (int, optional): skip first n items. Defaults to 0.
+		skip (int, optional): skip first n items. Defaults to 0.
 
-        limit (int, optional): maximum number of items. Defaults to 10.
-    Returns:
+		limit (int, optional): maximum number of items. Defaults to 10.
+	Returns:
 
-        list[dict[str, str]]: list of items
-    """
+		list[dict[str, str]]: list of items
+	"""
 
-    return fake_items_db[skip : skip + limit]
+	return fake_items_db[skip : skip + limit]
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: str, q: str | None = None) -> dict[str, str]:
-    """Get an item
 
-    Args:
+@app.get('/items/{item_id}')
+async def read_item(
+	item_id: str, q: str | None = None, short: bool = False
+) -> dict[str, str]:
+	"""Get an item
 
-        item_id (str): item id
+	Args:
 
-        q (str | None, optional): query. Defaults to None.
+		item_id (str): item id
 
-    Returns:
+		q (str | None, optional): query. Defaults to None.
 
-        dict[str, str]: item
-    """
-    if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
+	Returns:
 
+		dict[str, str]: item
+	"""
+	item = {'item_id': item_id}
+	if q:
+		item.update({'q': q})
+	if not short:
+		item.update(
+			{'description': 'This is an amazing item that has a long description'}
+		)
+	return item
