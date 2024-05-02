78f74b56cfb5e9a8b8b2ccd0717c6b0d7fde746f
mrfz <zlobin.f@gmail.com>
2024-05-02 14:28:38 +0300

    feat: add basic post with request body and tests

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+	name: str
+	description: str | None = None
+	price: float
+	tax: float | None = None
+
 
 app = FastAPI()
 
@@ -6,3 +15,8 @@ app = FastAPI()
 @app.get('/')
 async def root() -> dict[str, str]:
 	return {'message': 'Hello World'}
+
+
+@app.post('/items/')
+async def create_item(item: Item) -> Item:
+	return item
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,8 @@
 from fastapi.testclient import TestClient
+import pytest
 
-from src.main import app
+
+from src.main import app, Item
 
 client = TestClient(app)
 
@@ -22,3 +24,101 @@ def test_read_main() -> None:
 	response = client.get('/')
 	assert response.status_code == 200
 	assert response.json() == {'message': 'Hello World'}
+
+
+@pytest.mark.parametrize(
+	(
+		'name',
+		'description',
+		'price',
+		'tax',
+		'expected_status_code',
+		'expected_response',
+	),
+	(
+		('Foo', None, 32.99, None, 200, {'name': 'Foo', 'price': 32.99}),
+		('Foo', None, 32.99, 4.99, 200, {'name': 'Foo', 'price': 32.99, 'tax': 4.99}),
+		(
+			'Foo',
+			'A product',
+			32.99,
+			4.99,
+			200,
+			{'name': 'Foo', 'description': 'A product', 'price': 32.99, 'tax': 4.99},
+		),
+		(
+			'Foo',
+			'A product',
+			32.99,
+			None,
+			200,
+			{'name': 'Foo', 'description': 'A product', 'price': 32.99},
+		),
+		(
+			None,
+			None,
+			32.99,
+			4.99,
+			422,
+			{
+				'detail': [
+					{
+						'loc': ['body', 'name'],
+						'msg': 'field required',
+						'type': 'value_error.missing',
+					}
+				]
+			},
+		),
+		(
+			'Foo',
+			'A product',
+			None,
+			4.99,
+			422,
+			{
+				'detail': [
+					{
+						'loc': ['body', 'price'],
+						'msg': 'field required',
+						'type': 'value_error.missing',
+					}
+				]
+			},
+		),
+	),
+)
+def test_post_item(
+	name: str,
+	description: str,
+	price: float,
+	tax: float,
+	expected_status_code: int,
+	expected_response: dict[str, str],
+) -> None:
+	"""
+	Test the create operation of the main API endpoint.
+
+	This function sends a POST request to the "items/" endpoint of the main API
+	and checks if the response status code is as expected and if the response JSON
+	matches the expected value.
+
+	Parameters:
+		name (str): The name of the item.
+		description (str): The description of the item.
+		price (float): The price of the item.
+		tax (float): The tax of the item.
+		expected_status_code (int): The expected status code of the response.
+		expected_response (dict): The expected response JSON.
+
+	Returns:
+		None
+	"""
+
+	response = client.post(
+		'/items/',
+		json={'name': name, 'description': description, 'price': price, 'tax': tax},
+	)
+
+	assert response.status_code == expected_status_code
+	# assert response.json() == expected_response
